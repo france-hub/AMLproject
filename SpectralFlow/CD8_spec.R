6baa208f9d3db41f45c7c6bea19147c2fa5a42df
france-hub <francemazziotta@gmail.com>
2023-04-25 22:06:49 -0700

    Updated

--- SpectralFlow/CD8_spec.R
+++ SpectralFlow/CD8_spec.R
@@ -20,11 +20,7 @@
 #3) Run dimensionality reduction - PCA, UMAP and visualize
 #4) Add annotations and visualize
 
-##D) DA analysis using diffcyt
-#1) setup model formulas and contrast matrix
-#2) Run GLMM and plot
-
-##E) Trajectory inference
+##D) Trajectory inference
 #1) Use PCA as dimensionality reduction algorithm and run Slingshot
 #2) Visualize trajectories on first 3 PCs
 #3) Visualize subsets distribution along PT (jitter plot)
@@ -64,6 +60,7 @@ suppressPackageStartupMessages({
   library(ggpubr)
   library(scCustomize)
   library(patchwork)
+  library(tidyverse)
 })
 
 ##########################################################################################################
@@ -169,8 +166,6 @@ sce <- CATALYST::prepData(fs, panel = panel, md = md, transform = FALSE,
 
 sce@assays@data$exprs <- sce@assays@data$counts
 type_markers(sce) #visualize type markers
-#save(sce, file = "Spectral_test_CD8.rds")
-#load("Spectral_test_CD8.rds")
 
 #Rename conditions to make them consistent with scRNAseq labeling
 sce$condition <- ifelse(sce$condition == "CR_base", "Res_bas", 
@@ -184,6 +179,8 @@ n_cells(sce)
 plotCounts(sce, group_by = "sample_id", color_by = "condition")
 # Pseudo-bulk multidimensional scaling (pbMDS)
 CATALYST::pbMDS(sce, color_by = "patient_id")
+
+#Save Fig. S16
 # Non Redundancy Score (NRS)
 plotNRS(sce, features = type_markers(sce), color_by = "condition")
 # Filter out Ki67 (very low NRS)
@@ -214,49 +211,16 @@ sce <- runDR(sce, dr =  "UMAP", cells = n_cells, features = "type")
 sce <- runDR(sce, dr = "PCA", cells = n_cells, features = "type")
 
 # Plot UMAP
-#metaclusters distribution
-umap.meta8 <- plotDR(sce, "UMAP", color_by = "meta20", k_pal = pal_ident) + theme_void()
-p.coord <- ggplot(data.frame(x = 100, y = 100), aes(x = x, y = y)) +
-  geom_point() +
-  xlim(c(0, 10)) + ylim(c(0,10)) +
-  theme_classic() +
-  ylab("UMAP_2") + xlab("UMAP_1") +
-  theme(axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        axis.line = element_line(
-          arrow = arrow(angle = 15, length = unit(0.5, "cm"), type = "closed")))
-layout <- c(
-  area(t = 1, l = 3, b = 11, r = 11),
-  area(t = 10, l = 2, b = 12, r = 2)
-)
-umap.meta8 <- umap.meta8 + p.coord + plot_layout(design = layout)
-tiff("./WorkingDirectory/umap_8.tiff", width = 5*300, height = 5*300, res = 300, pointsize = 5)     
-umap.meta8
-dev.off()
-
 indx.mark <- match(c("CCR7", "TCF1", "CD127", "GZMK", "CD69", "CD57", 
                      "GZMB", "CX3CR1", "DNAM1"), type_markers(sce))
 
+#Save Fig. 7B
 tiff("./WorkingDirectory/umap_markers.tiff", width = 5*500, height = 5*500, res = 300, pointsize = 5)     
 plotDR(sce, "UMAP", color_by = type_markers(sce)[indx.mark], a_pal = pal_exp) +
   theme(strip.text = element_text(size=25)) +  xlab("UMAP_1") + ylab("UMAP_2") +
   theme(strip.text = element_text(size=25), axis.text=element_text(size=12),
         axis.title=element_text(size=14)) 
 dev.off()
-
-png("./WorkingDirectory/umap_clus.png", width = 5*900, height = 5*900, res = 300, pointsize = 5)     
-plotDR(sce, "UMAP", color_by = "meta8", facet_by = "group_id", k_pal = pal_ident) +
-  geom_point(size = 0.0000001)+
-  guides(color = guide_legend(ncol = 2, override.aes = list(size = 10))) + 
-  geom_density2d(binwidth = 0.006, colour = "black") +  
-  xlab("UMAP_1") + ylab("UMAP_2") +
-  theme(strip.text = element_text(size=40), axis.text=element_text(size=12),
-        axis.title=element_text(size=25)) + 
-  theme(legend.text=element_text(size=30)) 
-dev.off()
-
-plotDR(sce, "UMAP", color_by = "meta8", facet_by = "sample_id") #by sample_id
 
 #4) Add annotations and visualize
 #Read annotation file
@@ -272,94 +236,69 @@ sce <- mergeClusters(sce, k = "meta8",
 plotExprHeatmap(sce, features = "type", 
                 by = "cluster_id", k = "meta8", m = "cluster_annotation", scale = "last", bin_anno = TRUE, perc = TRUE)
 
-tiff("./WorkingDirectory/heat_def.tiff", width = 5*500, height = 5*250, res = 300, pointsize = 5)     
-plotExprHeatmap(sce, features = "type",  hm_pal = pal_exp, k_pal = pal_ident,
+heat <- plotExprHeatmap(sce, features = "type",  hm_pal = pal_exp, k_pal = pal_ident,
                 by = "cluster_id", k = "cluster_annotation", scale = "last") 
+
+#Save Fig. 7A
+tiff("./WorkingDirectory/heat_def.tiff", width = 5*500, height = 5*250, res = 300, pointsize = 5)     
+heat
 dev.off()
+
+#Extract median expressions to be used to build gene score for Int cells in scRNAseq
+df <- t(heat@matrix) %>% as.data.frame()
+df$Int <- round(df$Int, digits = 1)
+Int <- df %>% select(Int) %>% rownames_to_column()
+writexl::write_xlsx(Int, "Express_int.xlsx")
 
 #Plot UMAP
 tiff("./WorkingDirectory/umap_cond.tiff", width = 5*900, height = 5*900, res = 300, pointsize = 5)     
-plotDR(sce, "UMAP", color_by = "cluster_annotation", facet_by = "condition", k_pal = pal_ident) +
+p <- plotDR(sce, "UMAP", color_by = "cluster_annotation", facet_by = "condition") +
   guides(color = guide_legend(ncol = 1, override.aes = list(size = 10))) + 
   geom_density2d(binwidth = 0.006, colour = "black") +  
   xlab("UMAP_1") + ylab("UMAP_2") +
-  theme(strip.text = element_text(size=40), axis.text=element_text(size=12),
-        axis.title=element_text(size=25),
-        legend.text=element_text(size=30),
-        legend.title = element_text(size=0)) 
+  scale_colour_manual(values = pal_ident) +
+  theme(axis.title=element_text(size=0),
+        legend.position = "none",
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid.major = element_blank())
+
+splitFacet <- function(x){
+  facet_vars <- names(x$facet$params$facets)         
+  x$facet    <- ggplot2::ggplot()$facet 
+  datasets   <- split(x$data, x$data[facet_vars])    
+  new_plots  <- lapply(datasets,function(new_data) { 
+    x$data <- new_data
+    x})
+}    
+
+df <- p$data
+df <- aggregate(
+  x = df[c("x", "y")], 
+  by = list(z = df$cluster_annotation, w = df$condition), 
+  FUN = mean)
+df.split <- split(df, df$w)
+titles <- as.list(levels(as.factor(df$w)))
+p.list <- splitFacet(p)
+p.list <- lapply(seq_along(p.list), function(x) p.list[[x]] + 
+                   geom_label(
+                     aes(x, y, label = z),
+                     data = df.split[[x]], show.legend = FALSE,
+                     inherit.aes = FALSE,
+                     fill = NA,
+                     label.size = NA,
+                     size = 10) + ggtitle(titles[[x]]) +
+                   theme(plot.title = element_text(size = 27, hjust = 0.5, face = "bold"))) 
+
+
+#Save Fig. 7C
+tiff("./umap_AML.tiff", width = 5*800, height = 5*800, res = 300, pointsize = 5)     
+plot_grid(p.list[[1]], p.list[[3]], p.list[[2]], p.list[[4]], nrow = 2)
 dev.off()
 
-#Plot boxplot
-tiff("./WorkingDirectory/box20.tiff", width = 5*300, height = 5*180, res = 300, pointsize = 5)     
-p <- plotAbundances(sce, k = "cluster_annotation", by = "cluster_id")
-pal_box <- pal_ident[c(6,19, 14, 35)]
-p.box <- ggplot(p$data, aes(x = condition, y = Freq, fill = condition)) +
-  labs(x = NULL, y = "Proportion") + 
-  geom_boxplot() + scale_fill_manual(values= pal_box) +
-  geom_point(aes_string(x = "condition"), position = position_jitter(width = 0.2)) +
-  theme_bw() + theme(
-    panel.grid = element_blank(),
-    strip.text = element_text(face = "bold", size = 10),
-    strip.background = element_rect(fill = NA, color = NA),
-    axis.text = element_text(color = "black"),
-    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 10),
-    legend.key.height  =  unit(0.8, "lines")) +
-  facet_wrap(~ cluster_id, scales = "free_y", nrow = 1) + theme(legend.position = "none")
-p.box
-dev.off()
-
-##############################
-#D) DA analysis using diffcyt
-##############################
-
-#1) setup model formulas and contrast matrix
-ei <- sce@metadata$experiment_info
-ei <- ei %>%  mutate(group_id = case_when(grepl("NR", sample_id) ~ "NonRes",
-                                          grepl("CR", sample_id) ~ "Res")) 
-
-ei$group_id <- factor(ei$group_id)
-
-#Change condition names
-ei$condition <- factor(ifelse(ei$condition == "CR_base", "Res_bas", 
-                              ifelse(ei$condition == "NR_base", "NonRes_bas", 
-                                     ifelse(ei$condition == "CR_post", "Res_post", "NonRes_post"))))
-
-(da_formula <- createFormula(
-  ei, cols_fixed = "group_id", cols_random = "patient_id"))
-
-#Contrast Res vs NonRes
-contrast <- createContrast(c(0,1))
-
-#2) Run GLMM and plot
-out_DA1 <- diffcyt(sce, formula = da_formula, contrast = contrast, method_DA = "diffcyt-DA-GLMM",
-                   clustering_to_use = "cluster_annotation")
-da_1 <- topTable(out_DA1, format_vals = TRUE)
-
-tiff("./WorkingDirectory/heatmapSig.tiff", width = 5*900, height = 5*300, res = 300, pointsize = 5)     
-plotDiffHeatmap(sce, rowData(out_DA1$res), all = TRUE)
-dev.off()
-pal_box <- pal_ident[c(14, 35)]
-bxp <- plotAbundances(sce, k = "cluster_annotation", by = "cluster_id", group_by = "group_id")
-df.plot <- bxp$data %>% dplyr::mutate(positions = case_when(cluster_id == "StL" ~ "*", TRUE ~ ""))
-bxp <- ggplot(df.plot, aes(x = group_id, y = Freq, fill = group_id)) +
-  labs(x = NULL, y = "Proportion") + 
-  geom_boxplot() + scale_fill_manual(values= pal_box) +
-  geom_point(aes_string(x = "group_id"), position = position_jitter(width = 0.2)) +
-  theme_bw() + theme(
-    panel.grid = element_blank(),
-    strip.text = element_text(face = "bold", size = 15),
-    strip.background = element_rect(fill = NA, color = NA),
-    axis.text = element_text(color = "black"),
-    axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 15),
-    legend.key.height  =  unit(0.8, "lines")) +
-  facet_wrap(~ cluster_id, scales = "free_y", nrow = 1) + theme(legend.position = "none")
-bxp <- bxp + geom_text(aes(x=1.5,y=35,label=positions), size = 5)
-tiff("./WorkingDirectory/BoxplotSig.tiff", width = 5*600, height = 5*150, res = 300, pointsize = 5)     
-bxp
-dev.off()
 
 ########################
-#E Trajectory inference
+#D Trajectory inference
 ########################
 
 #1) Use PCA as dimensionality reduction algorithm and run Slingshot
@@ -385,6 +324,7 @@ curves <- setnames(curves, old = c('Dim.1','Dim.2', 'Dim.3'), new = c('PCA_1','P
 
 #2) Visualize trajectories on first 3 PCs
 pca.pseudo <- ggplot(df, aes(x = PCA_1, y = PCA_2)) +
+  guides(color = guide_legend(ncol = 1, override.aes = list(size = 10))) +
   geom_point(size = .7, aes(col = pst)) +
   scale_color_viridis_c() +
   labs(col = "Pseudotime") +
@@ -399,6 +339,7 @@ df$clusters <- cluster_ids(sce_sling, "cluster_annotation")
 pal_ident <- DiscretePalette_scCustomize(num_colors = 40, palette = "ditto_seq")
 
 p1.pca <- ggplot(df, aes(x = PCA_1, y = PCA_2)) +
+  guides(color = guide_legend(ncol = 1, override.aes = list(size = 10))) +
   geom_point(size = 0.0000001, aes(col = clusters)) +
   scale_colour_manual(values = pal_ident) +
   labs(col = "") +
@@ -414,6 +355,7 @@ p1.pca <- ggplot(df, aes(x = PCA_1, y = PCA_2)) +
   theme_classic()
 
 p2.pca <- ggplot(df, aes(x = PCA_1, y = PCA_3)) +
+  guides(color = guide_legend(ncol = 1, override.aes = list(size = 10))) +
   geom_point(size = 0.0000001, aes(col = clusters)) +
   scale_colour_manual(values = pal_ident) +
   labs(col = "") +
@@ -424,11 +366,12 @@ p2.pca <- ggplot(df, aes(x = PCA_1, y = PCA_3)) +
   guides(color = guide_legend(ncol = 2, override.aes = list(size = 10))) +
   theme(strip.text = element_text(size=40), axis.text=element_text(size=12),
         axis.title=element_text(size=25)) + 
-  theme(legend.text=element_text(size=30)) + 
+  theme(legend.text=element_text(size=40)) + 
   guides(color = guide_legend(override.aes = list(size = 10))) + 
   theme_classic()
 
 p3.pca <- ggplot(df, aes(x = PCA_2, y = PCA_3)) +
+  guides(color = guide_legend(ncol = 1, override.aes = list(size = 10))) +
   geom_point(size = 0.0000001, aes(col = clusters)) +
   scale_colour_manual(values = pal_ident) +
   labs(col = "") +
@@ -445,8 +388,9 @@ p3.pca <- ggplot(df, aes(x = PCA_2, y = PCA_3)) +
 
 p.pca.traj <- cowplot::plot_grid(pca.pseudo, p1.pca, p2.pca, p3.pca, ncol = 2)
 
-tiff("./WorkingDirectory/p.pca.traj.tiff", width = 5*300, height = 5*200, res = 150, pointsize = 5)     
-p.pca.traj
+#Save Fig. 7D
+tiff("./WorkingDirectory/p.pca.traj.tiff", width = 5*200, height = 5*100, res = 150, pointsize = 5)     
+p.pca.traj 
 dev.off()
 
 #3) Visualize subsets distribution along PT (jitter plot)
@@ -469,6 +413,7 @@ p.jitt2 <- ggplot(df,
   xlab("Pseudotime (ActEx)") +
   ylab("") + labs(col = "") + theme(legend.text=element_text(size=15))
 
+#Save Fig. 7E
 tiff("./WorkingDirectory/jitt.tiff", width = 5*300, height = 5*100, res = 150, pointsize = 5)     
 cowplot::plot_grid(p.jitt1, p.jitt2)
 dev.off()
@@ -521,6 +466,7 @@ umap.traj.ann <- ggplot(df.umap, aes(x = UMAP_1, y = UMAP_2)) +
 
 p.traj <- cowplot::plot_grid(umap.traj, umap.traj.ann, ncol = 1)
 
+#Save Fig. 7E
 tiff("./WorkingDirectory/umapTraj.tiff", width = 5*100, height = 5*150, res = 150, pointsize = 5)     
 p.traj + labs(x = "UMAP_1", y = "UMAP_2")
 dev.off()
